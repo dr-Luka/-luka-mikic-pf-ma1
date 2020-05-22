fd99964247e7f4ce8420cf650e6cb82830dae804
dr-Luka <63805178+dr-Luka@users.noreply.github.com>
2020-05-22 19:40:53 +0200

    Programming Foundations - MA 1

---
+++ script.js
@@ -0,0 +1,29 @@
+// question 1
+var firstVar;
+
+// question 2
+var name = "Luka";
+
+// question 3
+var age = 23;
+
+// question 4
+var division = 20 / 5;
+
+// question 5
+console.log(typeof "frog");
+
+// question 6
+var orderHasShipped = true;
+
+// question 7
+if (orderHasShipped === true) {
+  console.log("True");
+} else {
+  console.log("False");
+}
+
+// question 8
+for (i = 0; i <= 9; i += 1) {
+  console.log(i);
+}
